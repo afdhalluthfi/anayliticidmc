2694d17865457b5e4e8dfcd3de74c7abb329dfeb
yudi <jogjaplaza@gmail.com>
2021-12-23 15:51:51 +0700

    perubhaan

---
+++ template/resources/views/trash/sampah.blade.php
@@ -0,0 +1,45 @@
+{{-- <li class="nav-item {{ active_class(['/']) }}">
+      <a class="nav-link" href="{{ route('disokop.ukm') }}">
+        <i class="menu-icon mdi mdi-television"></i>
+        <span class="menu-title">UKM</span>
+      </a>
+    </li>
+    <li class="nav-item {{ active_class(['basic-ui/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#basic-ui" aria-expanded="{{ is_active_route(['basic-ui/*']) }}" aria-controls="basic-ui">
+        <i class="menu-icon mdi mdi-dna"></i>
+        <span class="menu-title">Basic UI Elements</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['basic-ui/*']) }}" id="basic-ui">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['basic-ui/buttons']) }}">
+            <a class="nav-link" href="{{ url('/basic-ui/buttons') }}">Buttons</a>
+          </li>
+          <li class="nav-item {{ active_class(['basic-ui/dropdowns']) }}">
+            <a class="nav-link" href="{{ url('/basic-ui/dropdowns') }}">Dropdowns</a>
+          </li>
+          <li class="nav-item {{ active_class(['basic-ui/typography']) }}">
+            <a class="nav-link" href="{{ url('/basic-ui/typography') }}">Typography</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+
+    <li class="nav-item {{ active_class(['charts/chartjs']) }}">
+      <a class="nav-link" href="{{ url('/charts/chartjs') }}">
+        <i class="menu-icon mdi mdi-chart-line"></i>
+        <span class="menu-title">Charts</span>
+      </a>
+    </li>
+    <li class="nav-item {{ active_class(['tables/basic-table']) }}">
+      <a class="nav-link" href="{{ url('/tables/basic-table') }}">
+        <i class="menu-icon mdi mdi-table-large"></i>
+        <span class="menu-title">Tables</span>
+      </a>
+    </li>
+    <li class="nav-item {{ active_class(['icons/material']) }}">
+      <a class="nav-link" href="{{ url('/icons/material') }}">
+        <i class="menu-icon mdi mdi-emoticon"></i>
+        <span class="menu-title">Icons</span>
+      </a>
+    </li> --}}
