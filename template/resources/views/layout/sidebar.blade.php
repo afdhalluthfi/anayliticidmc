89ac3a17623d2ef230347161b31f293260725ee8
yudi <jogjaplaza@gmail.com>
2021-12-23 14:45:09 +0700

    perubahan

--- template/resources/views/layout/sidebar.blade.php
+++ template/resources/views/layout/sidebar.blade.php
@@ -39,10 +39,10 @@
         </button>
       </div>
     </li>
-    <li class="nav-item {{ active_class(['/']) }}">
-      <a class="nav-link" href="{{ url('/') }}">
+    {{-- <li class="nav-item {{ active_class(['/']) }}">
+      <a class="nav-link" href="{{ route('disokop.ukm') }}">
         <i class="menu-icon mdi mdi-television"></i>
-        <span class="menu-title">Dashboard</span>
+        <span class="menu-title">UKM</span>
       </a>
     </li>
     <li class="nav-item {{ active_class(['basic-ui/*']) }}">
@@ -83,32 +83,271 @@
         <i class="menu-icon mdi mdi-emoticon"></i>
         <span class="menu-title">Icons</span>
       </a>
-    </li>
-    <li class="nav-item {{ active_class(['user-pages/*']) }}">
-      <a class="nav-link" data-toggle="collapse" href="#user-pages" aria-expanded="{{ is_active_route(['user-pages/*']) }}" aria-controls="user-pages">
+    </li> --}}
+    {{-- diskop ukm --}}
+    <li class="nav-item {{ active_class(['diskop-ukm/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#diskop-ukm" aria-expanded="{{ is_active_route(['diskop-ukm/*']) }}" aria-controls="user-pages">
         <i class="menu-icon mdi mdi-lock-outline"></i>
-        <span class="menu-title">User Pages</span>
+        <span class="menu-title">Diskop&UKM</span>
         <i class="menu-arrow"></i>
       </a>
-      <div class="collapse {{ show_class(['user-pages/*']) }}" id="user-pages">
+      <div class="collapse {{ show_class(['diskop-ukm/*']) }}" id="diskop-ukm">
         <ul class="nav flex-column sub-menu">
-          <li class="nav-item {{ active_class(['user-pages/login']) }}">
-            <a class="nav-link" href="{{ url('/user-pages/login') }}">Login</a>
+          <li class="nav-item {{ active_class(['diskop-ukm/koprasi']) }}">
+            <a class="nav-link" href="{{ route('disokop.koperasi') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
           </li>
-          <li class="nav-item {{ active_class(['user-pages/register']) }}">
-            <a class="nav-link" href="{{ url('/user-pages/register') }}">Register</a>
-          </li>
-          <li class="nav-item {{ active_class(['user-pages/lock-screen']) }}">
-            <a class="nav-link" href="{{ url('/user-pages/lock-screen') }}">Lock Screen</a>
+          <li class="nav-item {{ active_class(['diskop-ukm/ukm']) }}">
+            <a class="nav-link" href="{{ route('disokop.ukm') }}"><i class="menu-icon mdi mdi-box-shadow"></i> UKM</a>
           </li>
         </ul>
       </div>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="https://www.bootstrapdash.com/demo/star-laravel-free/documentation/documentation.html" target="_blank">
-        <i class="menu-icon mdi mdi-file-outline"></i>
-        <span class="menu-title">Documentation</span>
+    {{-- simpeg --}}
+    <li class="nav-item {{ active_class(['simpeg/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#simpeg" aria-expanded="{{ is_active_route(['simpeg/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">Simpeg</span>
+        <i class="menu-arrow"></i>
       </a>
+      <div class="collapse {{ show_class(['simpeg/*']) }}" id="simpeg">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['simpeg/golongan']) }}">
+            <a class="nav-link" href="{{ route('simpeg.golongan') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+          <li class="nav-item {{ active_class(['simpeg/umur']) }}">
+            <a class="nav-link" href="{{ route('simpeg.umur') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['simpeg/sppd']) }}">
+            <a class="nav-link" href="{{ route('simpeg.sppd') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['simpeg/pendidikan']) }}">
+            <a class="nav-link" href="{{ route('simpeg.pendidikan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simpeg/eselon']) }}">
+            <a class="nav-link" href="{{ route('simpeg.eselon') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Eselon</a>
+          </li>
+          <li class="nav-item {{ active_class(['simpeg/absen']) }}">
+            <a class="nav-link" href="{{ route('simpeg.absen') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Absen 3 Hari</a>
+          </li>
+          <li class="nav-item {{ active_class(['simpeg/pensiun']) }}">
+            <a class="nav-link" href="{{ route('simpeg.pensiun') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Yang Akan Pensiun</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- kependudukan --}}
+    <li class="nav-item {{ active_class(['kependudukan/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#kependudukan" aria-expanded="{{ is_active_route(['kependudukan/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">kependudukan</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['kependudukan/*']) }}" id="kependudukan">
+        <ul class="nav flex-column sub-menu">
+         
+          <li class="nav-item {{ active_class(['kependudukan/jenis']) }}">
+            <a class="nav-link" href="{{ route('kependudukan.jenis') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['kependudukan/agama']) }}">
+            <a class="nav-link" href="{{ route('kependudukan.agama') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['kependudukan/darah']) }}">
+            <a class="nav-link" href="{{ route('kependudukan.darah') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['kependudukan/perkawinan']) }}">
+            <a class="nav-link" href="{{ route('kependudukan.perkawinan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Eselon</a>
+          </li>
+          <li class="nav-item {{ active_class(['kependudukan/pekerjaan']) }}">
+            <a class="nav-link" href="{{ route('kependudukan.pekerjaan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Absen 3 Hari</a>
+          </li>
+          <li class="nav-item {{ active_class(['kependudukan/pendidikan']) }}">
+            <a class="nav-link" href="{{ route('kependudukan.pendidikan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Yang Akan Pensiun</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- e-lapor --}}
+    <li class="nav-item {{ active_class(['elapor/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#elapor" aria-expanded="{{ is_active_route(['elapor/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">elapor</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['elapor/*']) }}" id="elapor">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['elapor/kategori']) }}">
+            <a class="nav-link" href="{{ route('elapor.kategori') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['elapor/skpd']) }}">
+            <a class="nav-link" href="{{ route('elapor.skpd') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['elapor/kabupaten']) }}">
+            <a class="nav-link" href="{{ route('elapor.kabupaten') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['elapor/lokasi']) }}">
+            <a class="nav-link" href="{{ route('elapor.lokasi') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- survilance --}}
+    <li class="nav-item {{ active_class(['survilance/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#survilance" aria-expanded="{{ is_active_route(['survilance/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">survilance</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['survilance/*']) }}" id="survilance">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['survilance/atcs']) }}">
+            <a class="nav-link" href="{{ route('survilance.atcs') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/kota']) }}">
+            <a class="nav-link" href="{{ route('survilance.kota') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/bantul']) }}">
+            <a class="nav-link" href="{{ route('survilance.bantul') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/kp']) }}">
+            <a class="nav-link" href="{{ route('survilance.kp') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/public']) }}">
+            <a class="nav-link" href="{{ route('survilance.public') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/sleman']) }}">
+            <a class="nav-link" href="{{ route('survilance.sleman') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/sungai']) }}">
+            <a class="nav-link" href="{{ route('survilance.sungai') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+          <li class="nav-item {{ active_class(['survilance/malioboro']) }}">
+            <a class="nav-link" href="{{ route('survilance.malioboro') }}"><i class="menu-icon mdi mdi-box-shadow"></i>Koperasi</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- tataruang --}}
+    <li class="nav-item {{ active_class(['tataruang/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#tataruang" aria-expanded="{{ is_active_route(['tataruang/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">tataruang</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['tataruang/*']) }}" id="tataruang">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['tataruang/pola']) }}">
+            <a class="nav-link" href="{{ route('tataruang.pola') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['tataruang/ruang']) }}">
+            <a class="nav-link" href="{{ route('tataruang.ruang') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['tataruang/provinsi']) }}">
+            <a class="nav-link" href="{{ route('tataruang.provinsi') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- simangkis --}}
+    <li class="nav-item {{ active_class(['simangkis/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#simangkis" aria-expanded="{{ is_active_route(['simangkis/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">simangkis</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['simangkis/*']) }}" id="simangkis">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['simangkis/minum']) }}">
+            <a class="nav-link" href="{{ route('simangkis.minum') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/bab']) }}">
+            <a class="nav-link" href="{{ route('simangkis.bab') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/masak']) }}">
+            <a class="nav-link" href="{{ route('simangkis.masak') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/listrik']) }}">
+            <a class="nav-link" href="{{ route('simangkis.listrik') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/disabilitas']) }}">
+            <a class="nav-link" href="{{ route('simangkis.disabilitas') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/ijazah']) }}">
+            <a class="nav-link" href="{{ route('simangkis.ijazah') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/jenis']) }}">
+            <a class="nav-link" href="{{ route('simangkis.jenis') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/kepimilikan']) }}">
+            <a class="nav-link" href="{{ route('simangkis.kepimilikan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/kesataraan']) }}">
+            <a class="nav-link" href="{{ route('simangkis.kesataraan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/pekerjaan']) }}">
+            <a class="nav-link" href="{{ route('simangkis.pekerjaan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/kronis']) }}">
+            <a class="nav-link" href="{{ route('simangkis.kronis') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/perekawinan']) }}">
+            <a class="nav-link" href="{{ route('simangkis.perekawinan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['simangkis/penerangan']) }}">
+            <a class="nav-link" href="{{ route('simangkis.penerangan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- prajawibawa --}}
+    <li class="nav-item {{ active_class(['prajawibawa/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#prajawibawa" aria-expanded="{{ is_active_route(['prajawibawa/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">prajawibawa</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['prajawibawa/*']) }}" id="prajawibawa">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['prajawibawa/ketertiban']) }}">
+            <a class="nav-link" href="{{ route('prajawibawa.ketertiban') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['prajawibawa/pengamanan']) }}">
+            <a class="nav-link" href="{{ route('prajawibawa.pengamanan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['prajawibawa/pertolongan']) }}">
+            <a class="nav-link" href="{{ route('prajawibawa.pertolongan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['prajawibawa/perda']) }}">
+            <a class="nav-link" href="{{ route('prajawibawa.perda') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    {{-- disnaker --}}
+    <li class="nav-item {{ active_class(['disnaker/*']) }}">
+      <a class="nav-link" data-toggle="collapse" href="#disnaker" aria-expanded="{{ is_active_route(['disnaker/*']) }}" aria-controls="user-pages">
+        <i class="menu-icon mdi mdi-lock-outline"></i>
+        <span class="menu-title">disnaker</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse {{ show_class(['disnaker/*']) }}" id="disnaker">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item {{ active_class(['disnaker/angkatan']) }}">
+            <a class="nav-link" href="{{ route('disnaker.angkatan') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Golongan</a>
+          </li>
+          <li class="nav-item {{ active_class(['disnaker/usia']) }}">
+            <a class="nav-link" href="{{ route('disnaker.usia') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan Umur</a>
+          </li>
+          <li class="nav-item {{ active_class(['disnaker/penganggur']) }}">
+            <a class="nav-link" href="{{ route('disnaker.penganggur') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['disnaker/migran']) }}">
+            <a class="nav-link" href="{{ route('disnaker.migran') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+          <li class="nav-item {{ active_class(['disnaker/transmigrasi']) }}">
+            <a class="nav-link" href="{{ route('disnaker.transmigrasi') }}"><i class="menu-icon mdi mdi-box-shadow"></i> Berdasarkan SPPD</a>
+          </li>
+        </ul>
+      </div>
     </li>
   </ul>
 </nav>
--- template/routes/web.php
+++ template/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\KoperasiController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -134,7 +134,98 @@ Route::get('/clear-cache', function() {
     Artisan::call('cache:clear');
     return "Cache is cleared";
 });
-
+// diskop
+Route::group(['prefix'=>'diskop-ukm'],function(){
+    Route::get('koperasi','DashboardController@index')->name('disokop.koperasi');
+    Route::get('ukm', 'KoperasiController@index')->name('disokop.ukm');
+});
+//simpeg
+Route::group(['prefix'=>'simpeg'],function(){
+    Route::get('golongan','SimpegController@index')->name('simpeg.golongan');
+    Route::get('umur','SimpegController@index')->name('simpeg.umur');
+    Route::get('sppd','SimpegController@index')->name('simpeg.sppd');
+    Route::get('pendidikan','SimpegController@index')->name('simpeg.pendidikan');
+    Route::get('eselon','SimpegController@index')->name('simpeg.eselon');
+    Route::get('absem','SimpegController@index')->name('simpeg.absen');
+    Route::get('pensiun','SimpegController@index')->name('simpeg.pensiun');
+});
+// kependudukan
+Route::group(['prefix'=>'kependudukan'],function(){
+    Route::get('jenis-kelamin','KependudukanContorller@index')->name('kependudukan.jenis');
+    Route::get('agama','KependudukanContorller@index')->name('kependudukan.agama');
+    Route::get('golongan-darah','KependudukanContorller@index')->name('kependudukan.darah');
+    Route::get('perkawinan','KependudukanContorller@index')->name('kependudukan.perkawinan');
+    Route::get('pekerjaan','KependudukanContorller@index')->name('kependudukan.pekerjaan');
+    Route::get('pendidikan','KependudukanContorller@index')->name('kependudukan.pendidikan');
+});
+// e-lapor
+Route::group(['prefix'=>'e-lapor'],function(){
+    Route::get('kategori','ElaporContorller@index')->name('elapor.kategori');
+    Route::get('skpd','ElaporContorller@index')->name('elapor.skpd');
+    Route::get('kabupaten','ElaporContorller@index')->name('elapor.kabupaten');
+    Route::get('lokasi','ElaporContorller@index')->name('elapor.lokasi');
+});
+// survilance
+Route::group(['prefix'=>'survilance'],function(){
+    Route::get('atcs','SurvilanceContorller@index')->name('survilance.atcs');
+    Route::get('atcs-kota','SurvilanceContorller@index')->name('survilance.kota');
+    Route::get('cctv-bantul','SurvilanceContorller@index')->name('survilance.bantul');
+    Route::get('cctv-kp','SurvilanceContorller@index')->name('survilance.kp');
+    Route::get('cctv-public','SurvilanceContorller@index')->name('survilance.public');
+    Route::get('cctv-sleman','SurvilanceContorller@index')->name('survilance.sleman');
+    Route::get('cctv-sungai','SurvilanceContorller@index')->name('survilance.sungai');
+    Route::get('cctv-uptmalioboro','SurvilanceContorller@index')->name('survilance.malioboro');
+});
+//pertanahan
+Route::group(['prefix'=>'pertanahan'],function(){
+    Route::get('pertanahan','PetahananContorller@index')->name('pertahanan');
+});
+//tataruang
+Route::group(['prefix'=>'tataruang'],function(){
+    Route::get('pola-ruang','TataruangContorller@index')->name('tataruang.pola');
+    Route::get('struktur-ruang','TataruangContorller@index')->name('tataruang.ruang');
+    Route::get('strategi-provinsi','TataruangContorller@index')->name('tataruang.provinsi');
+});
+//simangkis
+Route::group(['prefix'=>'simangkis'],function(){
+    Route::get('air-minum','SimangkisContorller@index')->name('simangkis.minum');
+    Route::get('bab','SimangkisContorller@index')->name('simangkis.bab');
+    Route::get('bahan-masak','SimangkisContorller@index')->name('simangkis.masak');
+    Route::get('listrik','SimangkisContorller@index')->name('simangkis.listrik');
+    Route::get('disabilitas','SimangkisContorller@index')->name('simangkis.disabilitas');
+    Route::get('ijazah','SimangkisContorller@index')->name('simangkis.ijazah');
+    Route::get('jenis-dinding','SimangkisContorller@index')->name('simangkis.jenis');
+    Route::get('kepimilikan','SimangkisContorller@index')->name('simangkis.kepimilikan');
+    Route::get('kesataraan','SimangkisContorller@index')->name('simangkis.kesataraan');
+    Route::get('pekerjaan','SimangkisContorller@index')->name('simangkis.pekerjaan');
+    Route::get('pendidikan','SimangkisContorller@index')->name('simangkis.pendidikan');
+    Route::get('pekerjaan','SimangkisContorller@index')->name('simangkis.pekerjaan');
+    Route::get('penyakit-kronis','SimangkisContorller@index')->name('simangkis.kronis');
+    Route::get('perkawinan','SimangkisContorller@index')->name('simangkis.perekawinan');
+    Route::get('sumber-penerangan','SimangkisContorller@index')->name('simangkis.penerangan');
+});
+// covid
+Route::group(['prefix'=>'covid'],function(){
+    Route::get('sebaran-covid','CovidContorller@index')->name('covid.sebaran');
+    Route::get('statistik-covid','CovidContorller@index')->name('covid.statistik');
+    Route::get('info-rumahsakit','CovidContorller@index')->name('covid.rumahsakit');
+    Route::get('info-vaksin','CovidContorller@index')->name('covid.vaksin');
+});
+// prajawibawa
+Route::group(['prefix'=>'prajawibawa'],function(){
+    Route::get('ketertiban','PrajawibawaContorller@index')->name('prajawibawa.ketertiban');
+    Route::get('pengamanan','PrajawibawaContorller@index')->name('prajawibawa.pengamanan');
+    Route::get('pertolongan','PrajawibawaContorller@index')->name('prajawibawa.pertolongan');
+    Route::get('perda','PrajawibawaContorller@index')->name('prajawibawa.perda');
+});
+// disnaker
+Route::group(['prefix'=>'disnaker'],function(){
+    Route::get('angkatan-kerja','DisnakerContorller@index')->name('disnaker.angkatan');
+    Route::get('usia-kerja','DisnakerContorller@index')->name('disnaker.usia');
+    Route::get('penganggur','DisnakerContorller@index')->name('disnaker.penganggur');
+    Route::get('migran','DisnakerContorller@index')->name('disnaker.migran');
+    Route::get('transmigrasi','DisnakerContorller@index')->name('disnaker.transmigrasi');
+});
 // 404 for undefined routes
 Route::any('/{page?}',function(){
     return View::make('pages.error-pages.error-404');
